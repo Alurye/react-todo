dc7f25c8c12822e4017887e53ab479b21be8d486
Alurye <arthurlurye@gmail.com>
2023-06-06 12:27:50 -0400

    project created

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           React to do
+           React to do app
         </p>
        
       </header>
